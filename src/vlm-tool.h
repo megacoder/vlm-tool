046bb89e7f198629ace5cf5c2821d23e969672f2
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2012-05-25 20:46:05 -0500

    Moved post-processing into separate files.

---
+++ src/vlm-tool.h
@@ -0,0 +1,17 @@
+#ifndef	_VLM_TOOL_H
+#define	_VLM_TOOL_H
+#define	_GNU_SOURCE
+
+typedef	struct	trigger_s	{
+	char const *	s;		/* Original string		 */
+	regex_t		re;		/* Compiled regular expression	 */
+} trigger_t;
+
+typedef	struct	entry_s	{
+	trigger_t *	trigger;	/* We matched this trigger	 */
+	char *		resid;		/* Everything after host name	 */
+	size_t		host_id;	/* Index into hostname table	 */
+	time_t		timestamp;	/* Date for entry		 */
+} entry_t;
+
+#endif	/* _VLM_TOOL_H */
