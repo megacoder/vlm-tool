52c08537e56c9f444989837b8a31d7a7cb302972
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2012-11-09 14:02:11 -0600

    Don't archive generated files.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ if [ -d .git ]; then
 else
 	rm -f Makefile Makefile.in  vlm-tool.spec configure
 fi
+markdown2 README.md | tee README.html | lynx -dump -stdin >README
 autoreconf -vfim -I m4
 ./configure
 make dist
