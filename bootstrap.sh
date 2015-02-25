cd2d86878e603fee2a62a581c849ee41acf62221
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2015-02-25 15:49:36 -0600

    Try using "markdown2" else "markdown" else punt.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,11 @@ if [ -d .git ]; then
 else
 	rm -f Makefile Makefile.in  vlm-tool.spec configure
 fi
-markdown2 README.md | tee README.html | lynx -dump -stdin >README
+(
+	markdown2 README.md	||
+	markdown README.md	||
+	echo "Can't find a markdown renderer."
+) 2>/dev/null	| tee README.html | lynx -dump -stdin >README
 autoreconf -vfim -I m4
 ./configure
 make dist
